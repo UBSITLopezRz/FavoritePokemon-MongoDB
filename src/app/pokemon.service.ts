597dfe77c8280f72ca349f025d72e38e532c256d
lopez_z <lopez_z@ca01.ub.lan>
2026-03-18 15:21:43 +0800

    Created a new Angular with MongoDB

---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:3000/api/pokemon';
+
+  pokemonList = signal<any[]>([]);
+
+  fetchPokemon(){
+    this.http.get<any[]>(this.apiUrl).subscribe(data => this.pokemonList.set(data));
+  }
+
+  savePokemon(data: any){
+    return this.http.post(this.apiUrl, data);
+  }
+}
